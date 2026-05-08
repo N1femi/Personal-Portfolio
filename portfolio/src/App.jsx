3aa548c47e0fc86668eaf855963c6161e411c148
N1femi <niffybabs@gmail.com>
2026-05-07 20:06:06 -0400

    Some skeleton parts

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,8 +1,38 @@
+function Nav() {
+  return (
+    <>
+      <a id="home-btn" className="nav-btn"> Nifemi's WRLD</a>
+      <a className="nav-btn">Timeline</a>
+      <a className="nav-btn">Archives</a>
+      <a className="nav-btn">Resume</a>
+    </>
+  )
+}
+
+function IntroCard() {
+  return (
+    <>
+      <div id="intro-card">
+        <h3>
+          Hello!
+        </h3>
+
+        <h2>
+          My name is: { "Oluwanifemi Babajide" }
+        </h2>
+
+        <p>An aspiring Software, Systems, & AI/ML Engineer. </p>
+      </div>
+    </>
+  )
+}
+
 function App() {
   return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
+    <>
+      <Nav />
+      <IntroCard />
+    </>
   )
 }
 
